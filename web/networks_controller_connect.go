91fce007bef907dac9c77b05194e6b3093466997
Étienne Michon <etienne@scalingo.com>
2018-10-22 11:26:43 +0200

    HTTP 400 if ip or port are empty

--- web/networks_controller_connect.go
+++ web/networks_controller_connect.go
@@ -10,12 +10,25 @@ import (
 	"github.com/Scalingo/go-internal-tools/logger"
 	"github.com/docker/docker/pkg/reexec"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
 
 func (c NetworksController) Connect(w http.ResponseWriter, r *http.Request, params map[string]string) error {
 	w.Header().Set("Content-Type", "application/json")
 	ctx := r.Context()
 	log := logger.Get(ctx).WithField("network_id", params["id"])
+
+	ip := r.URL.Query().Get("ip")
+	port := r.URL.Query().Get("port")
+	log = log.WithFields(logrus.Fields{
+		"ip":   ip,
+		"port": port,
+	})
+	ctx = logger.ToCtx(ctx, log)
+	if ip == "" || port == "" {
+		w.WriteHeader(400)
+		return errors.New("IP and port are mandatory")
+	}
 
 	network, ok, err := c.NetworkRepository.Exists(ctx, params["id"])
 	if err != nil {
@@ -34,9 +47,6 @@ func (c NetworksController) Connect(w http.ResponseWriter, r *http.Request, para
 	defer socket.Close()
 
 	fmt.Fprintf(socket, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n")
-
-	ip := r.URL.Query().Get("ip")
-	port := r.URL.Query().Get("port")
 
 	tcpSocket := socket.(*net.TCPConn)
 	socketFile, err := tcpSocket.File()
