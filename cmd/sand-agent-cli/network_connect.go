802b1837675e33814895f671d40c90e7a8c7678d
Soulou <leo@unbekandt.eu>
2018-12-06 18:04:05 +0100

    Update CLI command network-connect, now able to start multiple connections

--- cmd/sand-agent-cli/network_connect.go
+++ cmd/sand-agent-cli/network_connect.go
@@ -24,14 +24,6 @@ func (a *App) NetworkConnect(c *cli.Context) error {
 		return errors.New("network, ip and port flags are mandatory")
 	}
 
-	conn, err := client.NetworkConnect(context.Background(), c.String("network"), params.NetworkConnect{
-		IP:   c.String("ip"),
-		Port: c.String("port"),
-	})
-	if err != nil {
-		return errors.Wrapf(err, "fail to connect to network %v", c.String("network"))
-	}
-
 	listener, err := net.Listen("tcp", ":")
 	if err != nil {
 		return errors.Wrapf(err, "fail to bind a socket")
@@ -40,45 +32,57 @@ func (a *App) NetworkConnect(c *cli.Context) error {
 
 	log.Infof("Waiting connection on %v", listener.Addr())
 
-	localConn, err := listener.Accept()
-	if err != nil {
-		return errors.Wrapf(err, "fail to accept connection")
+	for {
+		localConn, err := listener.Accept()
+		if err != nil {
+			return errors.Wrapf(err, "fail to accept connection")
+		}
+
+		go func(localConn net.Conn) {
+			conn, err := client.NetworkConnect(context.Background(), c.String("network"), params.NetworkConnect{
+				IP:   c.String("ip"),
+				Port: c.String("port"),
+			})
+			if err != nil {
+				log.WithError(err).Errorf("fail to connect to network %v", c.String("network"))
+			}
+
+			wg := &sync.WaitGroup{}
+			wg.Add(2)
+
+			closed := false
+			go func() {
+				defer func() {
+					closed = true
+					wg.Done()
+					localConn.Close()
+					conn.Close()
+				}()
+				log.Info("remote connection opened to the SAND network")
+				_, err := io.Copy(localConn, conn)
+				if err != io.EOF && err != nil && !closed {
+					log.WithError(err).Error("fail to copy data from local socket to remote network")
+				}
+				log.Info("remote connection closed to the SAND network")
+			}()
+
+			go func() {
+				defer func() {
+					closed = true
+					wg.Done()
+					localConn.Close()
+					conn.Close()
+				}()
+
+				_, err := io.Copy(conn, localConn)
+				if err != io.EOF && err != nil && !closed {
+					log.WithError(err).Error("fail to copy data from remote network to local socket")
+				}
+				log.Infof("local connection on %v closed", listener.Addr())
+			}()
+
+			wg.Wait()
+		}(localConn)
 	}
-
-	wg := &sync.WaitGroup{}
-	wg.Add(2)
-
-	closed := false
-	go func() {
-		defer func() {
-			closed = true
-			wg.Done()
-			localConn.Close()
-			conn.Close()
-		}()
-		log.Info("remote connection opened to the SAND network")
-		_, err := io.Copy(localConn, conn)
-		if err != io.EOF && err != nil && !closed {
-			log.WithError(err).Error("fail to copy data from local socket to remote network")
-		}
-		log.Info("remote connection closed to the SAND network")
-	}()
-
-	go func() {
-		defer func() {
-			closed = true
-			wg.Done()
-			localConn.Close()
-			conn.Close()
-		}()
-
-		_, err := io.Copy(conn, localConn)
-		if err != io.EOF && err != nil && !closed {
-			log.WithError(err).Error("fail to copy data from remote network to local socket")
-		}
-		log.Infof("local connection on %v closed", listener.Addr())
-	}()
-
-	wg.Wait()
 	return nil
 }
